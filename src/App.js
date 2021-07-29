1d19e590642f48d265acbbf209fa3d95c04ff691
unknown <programertv633@gmail.com>
2021-07-29 04:44:20 +0300

    make site more modern

--- src/App.js
+++ src/App.js
@@ -20,13 +20,22 @@ import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
 
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import AceEditor from "react-ace";
 
-import Button from '@material-ui/core/Button';
-import AddIcon from '@material-ui/icons/Add';
+import "ace-builds/src-noconflict/mode-markdown";
+import "ace-builds/src-noconflict/theme-monokai";
+
+import "ace-builds/src-noconflict/theme-kuroir";
+import "ace-builds/src-noconflict/theme-tomorrow";
+
+import "ace-builds/src-noconflict/ext-language_tools"
+
+
+import SaveIcon from '@material-ui/icons/Save';
+
+import Tooltip from '@material-ui/core/Tooltip';
+import ImageIcon from '@material-ui/icons/Image';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -235,196 +244,174 @@ function App() {
     }
   }, []);
 
+  const main_color = "rgb(57, 68, 70)";
 
   // Print when hit PRINT .PDF
   useEffect(() => {
     if (hidden) {
-      window.print();
+      console.log(document.getElementById("NOTICEME").getAttribute("val"));
+      if (document.getElementById("NOTICEME").getAttribute("val") == "1")
+      {
+        html2canvas(document.getElementById("NOTICEME") ).then(canvas => {
+          var link = document.createElement("a");
+          link.setAttribute('download', `math${Math.random().toString().split(".")[1]}.png`);
+          link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+          link.click();     
+          setHidden(false);    
+        })
+      }
+      else {
+        window.print();
+      }
     }
   }, [hidden])
 
-  const [val, setVal] = useState(0);
 
+
+  // Editor
+  const _grid = useRef();
+  const [_height, set_height] = useState(0);
+
+  useEffect(() => {
+    set_height(window.innerHeight - _grid.current.offsetTop);
+  }, [_grid])
+
+
+  const _render = useRef();
+  const [_rendered, set_rendered] = useState(0);
+
+  useEffect(() => {
+    set_rendered(window.innerHeight - _render.current.offsetTop);
+  }, [_render])
+
+  useEffect(() => {
+    const onResize = (event) => {
+      set_height(window.innerHeight - _grid.current.offsetTop);
+      set_rendered(window.innerHeight - _render.current.offsetTop);
+    }
+      
+    window.addEventListener("resize", onResize);
+    
+    return () => {
+      window.removeEventListener('resize', onResize);
+    }
+  }, [])
+
+
+
+
+  const handleChange = e => {
+    let val = e;
+    setTextValue(val);
+
+    // ([A-Za-z]|[^\x00-\x7F])(\d)
+    line_repl.forEach(i => {
+      val = line_replace(val, "\n" +i[0], "\n", i[1])
+    })
+
+    repl.forEach(i => {
+      val = fnd(val, i[0], i[1]);
+    })
+
+    val = replaceMathCenter(val, "$$");
+    val = replaceMath(val, "@@");
+
+    setValue(val);
+  }
+
+  const savePDF = () => {
+    document.getElementById("NOTICEME").setAttribute("val", "0")
+    setHidden(true);
+    setValue2(value);
+    localStorage.setItem("html", textValue);
+  }
+
+
+  const savePNG = () => {
+    document.getElementById("NOTICEME").setAttribute("val", "1");
+    setHidden(true);
+    setValue2(value);
+
+  }
 
 
   return (
     <div className="App">
+
       <div style={{display: hidden ? "none" : ""}}>
 
       <div style={{
         width: "100%",
-        height: "200px",
-        backgroundColor: "#393838",
+        height: "10px",
+        backgroundColor: "rgb(57, 68, 70)",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         position: "relative"
       
-      }}>
-
-        <div>
-          <div style={{
-            width : "98px",
-            height: "98px",
-            backgroundColor: "white",
-            borderRadius: "500px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-    
-          }}>
-
-          <FunctionsIcon style={{
-            width:  "64px",
-            height: "64px",
-            color: "rgb(57, 56, 56)"
-          }}/>  
-            
-          </div>          
-        </div>
-
-        <h2 style={{"marginLeft" : "30px", color: "aliceblue", fontFamily: "math2", }}>
-          Math Editor
-        </h2>
-
-
-        <Fab 
-        onClick={() => {
-          html2canvas(document.getElementById("peos").parentElement ).then(canvas => {
-            var link = document.createElement("a");
-            link.setAttribute('download', `math${Math.random().toString().split(".")[1]}.png`);
-            link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
-            link.click();          
-          })
-        }} 
-        
-        style={{position: "absolute", marginRight: "10px", marginBottom: "10px", right: 0, bottom: 0}} variant="extended" color="inherit" aria-label="add">
-          <SaveAltIcon style={{"animation" : "none"}} />
-          <span style={{marginLeft: "10px", fontWeight: "700", fontFamily: "math"}}>
-            SAVE .PNG
-          </span>
-        </Fab>
-
-
-
-        <Fab 
-        onClick={() => {
-          setHidden(true);
-          setValue2(value);
-          localStorage.setItem("html", textValue);
-           
-        
-        }} 
-        
-        style={{position: "absolute", marginLeft: "10px", marginBottom: "10px", left: 0, bottom: 0}} variant="extended" color="inherit" aria-label="add">
-          <PictureAsPdfIcon style={{"animation" : "none"}} />
-          <span style={{marginLeft: "10px", fontWeight: "700", fontFamily: "math"}}>
-            PRINT .PDF
-          </span>
-        </Fab>
-
+      }}
+      
+      
+      onWheel={() => console.log("scroll")}>
 
 
       </div>
 
+      <div style={{
+      position: "absolute",
+      right: 0,
+      bottom: 0,
+      display: "grid"
+      }}>
+        <Fab onClick={() => savePDF()} style={{marginBottom: "10px", marginRight: "10px"}} color="secondary" aria-label="add">
+          <PictureAsPdfIcon />    
+        </Fab>
+
+        <Fab onClick={() => savePNG()} style={{marginBottom: "10px", marginRight: "10px"}} color="primary" aria-label="add">
+          <ImageIcon />
+        </Fab>
+      </div>
 
 
-    <AppBar position="static">
-      <Tabs TabIndicatorProps={{style: {background:'white'}}} value={val} onChange={(e) => console.log(e)} aria-label="simple tabs example">
-        {/* <Tab onClick={() => setVal(0)} label="Untitled"  /> */}
-        {/* <Tab onClick={() => setVal(1)} label="Untitled2"  /> */}
-
-        {/* <Button variant="primary">
-          <AddIcon style={{width: "16px", height: "16px", color: "white"}} />
-        </Button> */}
-      </Tabs>
-    </AppBar>
 
 
-      <Grid container spacing={3}>
-        <Grid item xs={6}>
-        <textarea
-        value={textValue}
-      style={{
-        "width" : "100%",
-        height : "500px",
-        fontSize: "13px",
-        resize: "vertical",
-        float: "left",
-        // border: "1px solid rgb(57, 56, 56)",
-        // fontSize: "20px"
-        
-      }}
-      
-      onChange={(e) => {
-        
-        let val = e.currentTarget.value;
-        setTextValue(val);
+      <Grid style={{ }} container spacing={3}>
+        <Grid ref={_grid} style={{borderRight: `10px solid ${main_color}`, paddingBottom: 0, paddingRight: 0, height: _height}} item xs={6}>
+          <AceEditor  
+          value={textValue}
+          style={{width: "100%"}}
 
-        // ([A-Za-z]|[^\x00-\x7F])(\d)
-        line_repl.forEach(i => {
-          val = line_replace(val, "\n" +i[0], "\n", i[1])
-        })
+          onChange={e => handleChange(e)}
+          
+          fontSize={15}
+          height={_height}
+          placeholder={`Start typing some math. Syntax rules can be found in https://github.com/Greece4ever/Math-Editor`}
+          mode="markdown"
+          theme="monokai"
+          name="blah2" />
 
-        repl.forEach(i => {
-          val = fnd(val, i[0], i[1]);
-        })
-
-        val = replaceMathCenter(val, "$$");
-        val = replaceMath(val, "@@");
-
-        setValue(val);
-        
-      }}
-
-      onKeyDown={(e) => {
-          let target = e.currentTarget;
-          let value = target.value;
-          switch (e.key.toLocaleLowerCase()) {
-            case "tab":
-              e.preventDefault();
-              target.value = insert_at(target.value, "    ", target.selectionStart)
-              console.log(target.value);
-              break;
-            case "shift":
-              break;
-          }
-
-      }}></textarea>
 
         </Grid>
-        <Grid item xs={6}>
+
+        <Grid style={{height: _rendered, paddingBottom: 0, borderLeft: `10px solid ${main_color}`}} ref={_render} item xs={6}>
           <div id="peos" style={{
             width: "100%",
             background: "white",
             float: "right",
-            marginTop: " 10px",
-            fontFamily: "math",
-            minHeight: "800px",
-            margin: "10px",
-            overflowWrap: "break-word",
-    
 
-            
+            marginTop: "10px",
+            fontFamily: "math",
+            height: "calc(100% - 10px)",
+
+            overflow: "auto"
           }}
-          dangerouslySetInnerHTML={{__html: value}}
-          ></div>
+
+          dangerouslySetInnerHTML={{__html: value}}></div>
         </Grid>
       </Grid>
-
-      
       </div>
 
-
-      <div id="NOTICEME" style={{display: hidden ? "block" : "none", fontFamily: "math", margin: "10px"}}
-      
-      
-      
-      dangerouslySetInnerHTML={{__html: value2}}
-      >
-        
-      </div>
-
+        <div val="0" id="NOTICEME" style={{display: hidden ? "block" : "none", fontFamily: "math", margin: "10px", minHeight: window.innerHeight}} dangerouslySetInnerHTML={{__html: value2}}></div>
     </div>
   );
 }
